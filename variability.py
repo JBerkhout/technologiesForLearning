c3cd93449d38f81fb7f18cfae0ec4b5abf639535
AlissaVermast <alissavermast@gmail.com>
2021-10-19 07:43:57 +0200

    variability is now done.

--- variability.py
+++ variability.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
+
 
 def compute_variability_statistics():
     data_dict = pd.read_excel("data_v2.xlsx", None)
@@ -25,8 +25,7 @@ def compute_variability_statistics():
     return output
 
 
-
-def read_variability_statistics():
+def read_rubric_variability_statistics():
     # Let's infer some information from the statistics.xlsx
     variability_data = pd.read_excel("variability_statistics.xlsx", None)
     df = variability_data['Sheet1']
@@ -60,16 +59,57 @@ def read_variability_statistics():
     return output
 
 
+def read_topic_variability_statistics():
+    # Let's infer some information from the statistics.xlsx
+    variability_data = pd.read_excel("variability_statistics.xlsx", None)
+    df = variability_data['Sheet1']
+    output = []
+
+    for topic in range(1, 23):
+        topic_selection = df.loc[df['topic'] == topic]
+
+        # First we compute the total mean value that is given for each topic.
+        means_list = topic_selection['mean'].tolist()
+        total_mean = np.mean(means_list)
+
+        # Now let's see if students agree more with each other (less variability) on certain topics.
+        std_list = topic_selection['std'].tolist()
+        mean_std = np.mean(std_list)
+        # So this value above is the average standard deviation for each rubric.
+
+        # Let's do the same for variance, in case we want to use that instead of std.
+        variance_list = topic_selection['variance'].tolist()
+        mean_variance = np.mean(variance_list)
+        # So this value above is the average variance for each rubric.
+
+        # Now let's save all this information per rubric in a dictionary.
+        rubric_variablility = {
+                        "topic": topic,
+                        "total_mean": total_mean,
+                        "mean_std": mean_std,
+                        "mean_variance": mean_variance}
+        output.append(rubric_variablility)
+
+    return output
+
+
 def save_statistics_excel():
     out = compute_variability_statistics()
     df = pd.DataFrame.from_dict(out)
     df.to_excel('variability_statistics.xlsx')
 
+
 def save_rubric_variability_excel():
-    out = read_variability_statistics()
+    out = read_rubric_variability_statistics()
     df = pd.DataFrame.from_dict(out)
     df.to_excel('rubric_variability.xlsx')
 
 
+def save_topic_variability_excel():
+    out = read_topic_variability_statistics()
+    df = pd.DataFrame.from_dict(out)
+    df.to_excel('topic_variability.xlsx')
+
+
 #save_statistics_excel()
-save_rubric_variability_excel()
+save_topic_variability_excel()
