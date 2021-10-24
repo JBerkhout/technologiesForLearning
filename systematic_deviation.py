da0e1cf37e5c9fc7f72394eca5f224577d5d80ef
JBerkhout <32328759+JBerkhout@users.noreply.github.com>
2021-10-24 10:18:53 +0200

    Added (a form of) std deviation

--- systematic_deviation.py
+++ systematic_deviation.py
@@ -6,14 +6,22 @@ import matplotlib.pyplot as plt
 
 from pre_processing import get_short_rubrics_names_dict, get_topics_names_dict
 
+# Returns a tuple with two arrays containing the high/low bias and the std bias for each reviewer respectively
 def compute_systematic_deviation_statistics():
     data_dict = pd.read_excel("data_v2.xlsx", None)
-    print (sys_high_low(data_dict))
 
-# High/low
-def sys_high_low(data_dict):
+    grades_list = list_grades_per_reviewer(data_dict)
+    high_low = sys_high_low(grades_list)
+    spread = sys_spread(grades_list)
+    
+    # print(high_low)
+    # print(spread)
+    return(high_low, spread)
+
+# Calculates the systematic deviation for each reviewer. >0 values means positive bias, <0 value means negative bias
+def sys_high_low(grades_list):
     # Calculate the mean grade assigned by each reviewer
-    reviewer_grades = list_grades_per_reviewer(data_dict)
+    reviewer_grades = grades_list
     reviewer_means = np.zeros(reviewer_grades.__len__())
     i = 0
     for reviewer in reviewer_grades:
@@ -22,7 +30,6 @@ def sys_high_low(data_dict):
 
     # For each reviewer, calculate the difference between their mean and the mean of the other means
     reviewer_bias = np.zeros(reviewer_means.__len__())
-
     i = 0
     while i < reviewer_bias.__len__():
         # Loop over all reviewers, make a list with all reviewers except the current one
@@ -34,13 +41,39 @@ def sys_high_low(data_dict):
                 continue
             otherReviewers.append(reviewer_means[j])
             j += 1
+        # Calculate the individual systematic bias
         reviewer_bias[i] = reviewer_means[i] - average(otherReviewers)
         i += 1
     return(reviewer_bias)
 
-# Broad/narrow (amplitude)
-def sys_range():
-    print()
+# Calculate the difference in std compared to the average std of the rest of the reviewers
+def sys_spread(grades_list):
+    # Start by calculating the standard deviation per reviewer
+    std_per_reviewer = np.zeros(grades_list.__len__())
+    h = 0
+    for reviewer in grades_list:
+        std_per_reviewer[h] = np.std(reviewer, ddof=1)
+        h += 1
+    
+    # print(std_per_reviewer)
+
+    # For each reviewer, calculate the difference between their standard deviation and the mean of the other standard deviations
+    reviewer_range = np.zeros(std_per_reviewer.__len__())
+    i = 0
+    while i < reviewer_range.__len__():
+        # Loop over all reviewers, make a list with all reviewers std except the current one
+        otherReviewers = []
+        j = 0
+        while j < std_per_reviewer.__len__():
+            if(j == i or math.isnan(std_per_reviewer[j])):
+                j += 1
+                continue
+            otherReviewers.append(std_per_reviewer[j])
+            j += 1
+        # Calculate the systematic range bias
+        reviewer_range[i] = std_per_reviewer[i] - average(otherReviewers)
+        i += 1
+    return reviewer_range
 
 def average(list):
     len = list.__len__()
@@ -48,6 +81,8 @@ def average(list):
         return None
     return (sum(list) / len)
 
+# Returns an array containing for each reviewer a list with all grades they have given
+# If a reviewer never handed in any grades, nan is put in place instead
 def list_grades_per_reviewer(data_dict):
     # Create and initialize array of empty lists to store grades in
     reviewer_grades = np.empty(44, dtype=list)
